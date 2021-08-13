81bf2ce66ea8afc471698a31ff2f4ccdde860280
Abiddhia <muhammadabiddhia@gmail.com>
2021-08-13 13:19:59 +0700

    membuat seeder dan migration oleh abid

---
+++ database/migrations/2021_08_13_063038_create_suppliers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name');
+            $table->text('email');
+            $table->text('address');
+            $table->text('phone');
+            $table->text('image');
+            $table->softDeletes('deleted_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
