de2427f58a042b156f0209ce2a0f99ca1dc350d5
Mochammad Gentar <Gtrabimanyu@gmail.com>
2021-08-24 13:00:58 +0700

    update membuat coontroller dan model oleh: Gentar

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\controllers\purchasesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,4 @@ use App\Http\Controllers\DashboardController;
 */
 
 Route::get('/', [DashboardController::class, 'index']);
+Route::get('/purchases', [purchasesController::class, 'index']);
